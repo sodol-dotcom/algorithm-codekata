1635bf45c4f57c4fe063b17266a4471499c7e640
sodol <sodol@macpro.local>
2024-08-09 10:04:53 +0900

    01_두 수의 차

---
+++ 01_SubTwoNumbers.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+    var answer = 0
+    return answer = num1 - num2;
+}
+
+console.log(solution(2, 3));   // Output: -1
+console.log(solution(100, 2)); // Output: 98
