98b180d9f9c463462ae6866cea08f40ec0f0db25
sodol <sodol@macpro.local>
2024-08-12 09:50:52 +0900

    10_배열의 평균값

---
+++ 10_ArrayAverage.js.js
@@ -0,0 +1,14 @@
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+    let sum = 0;
+    
+    // 배열의 요소를 순회하면서 합을 구함
+    for ( let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    
+    // 배열의 요소의 합을 구하고 배열의 길이(요소의 개수)로 나눔
+    average = sum / numbers.length;
+    return average;
+}
--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    answer = 0;
-    for (var i = 0; i <= n; i++) {
-        if(i / 2 === 0) {
-            answer = answer + i;
-        }
-    }
-    return answer;
-}
